d38cf194ea8c814df3d5d76dcc8ce12e85929068
Alex <alexgoffa157@gmail.com>
2025-05-31 19:50:21 +0700

    new design

--- website/src/app/pages/Home.tsx
+++ website/src/app/pages/Home.tsx
@@ -1,18 +1,10 @@
-import { SanityDocument } from "@sanity/client";
-import { RequestInfo } from "rwsdk/worker";
-import { client } from "../lib/sanity/client";
+import { SimpleHeroSection } from "../components/SimpleHeroSection";
 
-const POSTS_QUERY = `*[
-  _type == "post"
-  && defined(slug.current)
-]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
-
-export async function Home({ ctx }: RequestInfo) {
-  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY);
-
+export function Home() {
   return (
-    <div>
-      <pre>{JSON.stringify(posts, null, 2)}</pre>
-    </div>
+    <main>
+      <SimpleHeroSection />
+      {/* You can add more sections here later */}
+    </main>
   );
 }
--- website/src/client.tsx
+++ website/src/client.tsx
@@ -1,3 +1,4 @@
 import { initClient } from "rwsdk/client";
+// import 'flowbite';
 
 initClient();
